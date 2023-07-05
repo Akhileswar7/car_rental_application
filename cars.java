1e608c610b2a21714a9f4b4a536434afe695cf63
Akhileswar7 <131006031+Akhileswar7@users.noreply.github.com>
2023-07-05 21:01:12 +0530

    Add files via upload

---
+++ cars.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class cars extends rent 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,car;
+        double dist;
+        String[] carsavail={"Sedan","HatchBack","5 Seater"};
+        for(i=0;i<carsavail.length;i++){
+            System.out.println((i+1)+" "+carsavail[i]);
+        }
+        System.out.println("Select a car to ride");
+        car=sc.nextInt();
+        cars obj=new cars();
+        switch(car){
+            case 1:
+            System.out.print("Enter Ride Distance :");
+            dist = sc.nextDouble();
+            obj.sedan(dist);
+            break;
+            case 2:
+            System.out.print("Enter Ride Distance :");
+            dist = sc.nextDouble();
+            obj.hatchback(dist);
+            break;
+            case 3:
+            System.out.print("Enter Ride Distance :");
+            dist = sc.nextDouble();
+            obj.seater(dist);
+            break;
+        }
+    }
+}
